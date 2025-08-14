f9ed0d0f470855a0f60520984b4cf861b926c4e5
CharlesJRM <140403739+CharlesJRM@users.noreply.github.com>
2025-08-13 20:03:20 -0700

    Implementar función para generar árbol de diferencias

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,3 +1,23 @@
-# Se implementará en el paso 5
+from gendiff.parser import parse
+
 def generate_diff(file_path1, file_path2):
-    raise NotImplementedError("generate_diff() se implementará en el paso 5")
+    data1 = parse(file_path1)
+    data2 = parse(file_path2)
+    return _build_diff(data1, data2)
+
+def _build_diff(data1, data2):
+    keys = sorted(data1.keys() | data2.keys())
+    diff = []
+
+    for key in keys:
+        if key not in data1:
+            diff.append(('added', key, data2[key]))
+        elif key not in data2:
+            diff.append(('removed', key, data1[key]))
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            diff.append(('nested', key, _build_diff(data1[key], data2[key])))
+        elif data1[key] != data2[key]:
+            diff.append(('changed', key, data1[key], data2[key]))
+        else:
+            diff.append(('unchanged', key, data1[key]))
+    return diff
