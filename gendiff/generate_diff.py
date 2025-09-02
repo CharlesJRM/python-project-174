b6ab1d17777c1739560584c805de9eae715a67ea
CharlesJRM <140403739+CharlesJRM@users.noreply.github.com>
2025-09-02 13:52:15 -0700

    Resuelve conflictos tras pull

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,23 +1,37 @@
 import json
-from gendiff.utils import to_str
+
+
+def format_value(value):
+    """Formatea valores para que coincidan con JSON (true/false para bool)."""
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
 
 
 def generate_diff(file_path1, file_path2):
-    with open(file_path1) as f1, open(file_path2) as f2:
-        dict1 = json.load(f1)
-        dict2 = json.load(f2)
+    # Leer archivos JSON
+    with open(file_path1) as f1:
+        data1 = json.load(f1)
+    with open(file_path2) as f2:
+        data2 = json.load(f2)
 
-    diff = ['{']
-    keys = sorted(set(dict1.keys()) | set(dict2.keys()))
-    for key in keys:
-        if key in dict1 and key not in dict2:
-            diff.append(f"  - {key}: {to_str(dict1[key])}")
-        elif key not in dict1 and key in dict2:
-            diff.append(f"  + {key}: {to_str(dict2[key])}")
-        elif dict1[key] == dict2[key]:
-            diff.append(f"    {key}: {to_str(dict1[key])}")
+    # Todas las claves únicas ordenadas
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+
+    diff_lines = ["{"]
+    for key in all_keys:
+        val1 = format_value(data1[key]) if key in data1 else None
+        val2 = format_value(data2[key]) if key in data2 else None
+
+        if key in data1 and key not in data2:
+            diff_lines.append(f"  - {key}: {val1}")
+        elif key not in data1 and key in data2:
+            diff_lines.append(f"  + {key}: {val2}")
+        elif val1 == val2:
+            diff_lines.append(f"    {key}: {val1}")
         else:
-            diff.append(f"  - {key}: {to_str(dict1[key])}")
-            diff.append(f"  + {key}: {to_str(dict2[key])}")
-    diff.append('}')
-    return '\n'.join(diff)
+            diff_lines.append(f"  - {key}: {val1}")
+            diff_lines.append(f"  + {key}: {val2}")
+    diff_lines.append("}")
+
+    return "\n".join(diff_lines)
