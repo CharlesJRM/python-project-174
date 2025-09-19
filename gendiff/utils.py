9159ccda5005cde67fe4189041d3db62e8390b05
CharlesJRM <140403739+CharlesJRM@users.noreply.github.com>
2025-09-18 19:30:34 -0700

    Update utils.py

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -1,6 +1,9 @@
-def test_to_str():
-    assert to_str(True) == "true"
-    assert to_str(False) == "false"
-    assert to_str(None) == "null"
-    assert to_str(123) == "123"
-    assert to_str("hello") == "hello"
+def to_str(value):
+    if value is True:
+        return "true"
+    elif value is False:
+        return "false"
+    elif value is None:
+        return "null"
+    else:
+        return str(value)
